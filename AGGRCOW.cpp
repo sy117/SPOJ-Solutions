243580a540398b736c8d2fbc2bcc87bf6af3bf05
Sunil Kumar <07sunilraj@gmail.com>
2018-01-09 22:52:55 +0530

    Add files via upload

---
+++ AGGRCOW.cpp
@@ -0,0 +1,98 @@
+/*
+	          ********************
+                                                 
+*/
+
+#include <bits/stdc++.h>
+#include <iostream>
+#define MOD 1000000007//;
+#define MAX 100005
+#define FIO ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define T() int t; scanf("%d",&t);while(t--)
+#define REP(i,a,b) for(int (i) = (a); (i) < (b); (i)++)
+#define rep(i,n) for(int (i) = (0); (i) < (n); (i)++)
+#define REV(i,a,b) for(int (i) = (a)-1; (i) >= (b); (i)--)
+#define CLEAR(a) memset((a),0,sizeof(a))
+#define all(v) v.begin(), v.end()
+#define si(x) scanf("%d",&x)
+#define sii(x,y) scanf("%d%d",&x,&y);
+#define sl(x) scanf("%lld",&x)
+#define sll(x,y) scanf("%lld%lld",&x,&y);
+#define pi(x) printf("%d\n",x)
+#define pl(x) printf("%lld\n",x)
+#define lb lower_bound
+#define ub upper_bound
+#define bs binary_search
+#define fi first
+#define se second
+#define pb push_back
+#define mp make_pair
+#define sz size
+#define ln length
+#define itr iterator;
+
+using namespace std;
+
+typedef long long int LL;
+typedef vector<int> VI;
+typedef vector<LL> VL;
+typedef pair<int,int> PI;
+typedef pair<LL,LL> PL;
+
+template<typename T> T gcd(T a, T b){return (b?__gcd(a,b):a);}
+template<typename T> T lcm(T a, T b){return (a*(b/gcd(a,b)));}
+
+bool check(LL a[],LL n,LL c,LL x)
+{
+	LL curr,count;
+	curr = a[0];
+	count=1;
+	for(LL i=1;i<n;i++)
+	{
+		if(a[i]>=curr+x)
+		{
+			curr=a[i];
+			count++;
+			if(count==c)
+			return true;
+		}
+		
+	}
+	if(count>=c)
+	return true;
+	else
+	return false;
+}
+int main() 
+{
+	T()
+	{
+		LL n,c;
+		sll(n,c);
+		LL a[n];
+		for(LL i=0;i<n;i++)
+		{
+			sl(a[i]);
+		}
+		sort(a,a+n);
+		LL lo,hi,mid,ans;
+		lo = 0;
+		hi = a[n-1];
+		ans=-1;
+		while(lo<=hi)
+		{
+			mid = lo+(hi-lo)/2;
+			if(check(a,n,c,mid))
+			{
+				ans = mid;
+				lo = mid+1;
+			}
+			else
+			{
+				hi = mid-1;
+			}
+		}
+		pl(ans);
+	}
+	return 0;
+}
